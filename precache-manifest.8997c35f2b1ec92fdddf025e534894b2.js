5f799ac4c10f00c4040cb1d37680801d6c11ed06
Pumai Subanakyot <pumaisub97@gmail.com>
2022-07-21 18:28:29 -0700

    Updates

---
+++ precache-manifest.8997c35f2b1ec92fdddf025e534894b2.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4b4f6bd976641e7e522",
+    "url": "/warren-portfolio-router/static/css/main.cf736655.chunk.css"
+  },
+  {
+    "revision": "b4b4f6bd976641e7e522",
+    "url": "/warren-portfolio-router/static/js/main.b4b4f6bd.chunk.js"
+  },
+  {
+    "revision": "904c1417d70eb27f5cda",
+    "url": "/warren-portfolio-router/static/js/1.904c1417.chunk.js"
+  },
+  {
+    "revision": "9dda4ef7b782a6ca95d1",
+    "url": "/warren-portfolio-router/static/js/runtime~main.9dda4ef7.js"
+  },
+  {
+    "revision": "424cadf3f7b00e27edd49ecf6b76b1b1",
+    "url": "/warren-portfolio-router/static/media/horiz2.424cadf3.jpg"
+  },
+  {
+    "revision": "a8a87ea1a6c03a03c9c2fb4747db8c5e",
+    "url": "/warren-portfolio-router/static/media/horiz1.a8a87ea1.jpg"
+  },
+  {
+    "revision": "38522a972635d556a539c53ea3dfae14",
+    "url": "/warren-portfolio-router/static/media/vert18.38522a97.jpg"
+  },
+  {
+    "revision": "eb3b88a96aaf31f9c425378eed1e0bf4",
+    "url": "/warren-portfolio-router/static/media/vert19.eb3b88a9.jpg"
+  },
+  {
+    "revision": "ca29b38765b654e4a12653662f6117c7",
+    "url": "/warren-portfolio-router/static/media/vert20.ca29b387.jpg"
+  },
+  {
+    "revision": "614a4b4e93468695ae48fab060e39ecf",
+    "url": "/warren-portfolio-router/static/media/vert21.614a4b4e.jpg"
+  },
+  {
+    "revision": "d006c8df9e958c5f42edd47ef38a4d5d",
+    "url": "/warren-portfolio-router/static/media/vert22.d006c8df.jpg"
+  },
+  {
+    "revision": "9fe4c54190fb453284d1bdf1253f9738",
+    "url": "/warren-portfolio-router/static/media/vert1.9fe4c541.jpg"
+  },
+  {
+    "revision": "40b25b7f26d86ea4a704f9d1819d59e2",
+    "url": "/warren-portfolio-router/static/media/vert1.40b25b7f.jpg"
+  },
+  {
+    "revision": "fbdcda722bb68c1c42c9fe9d86882745",
+    "url": "/warren-portfolio-router/static/media/GradVert1.fbdcda72.jpg"
+  },
+  {
+    "revision": "87da8090cf54d749bc01f804e206ef10",
+    "url": "/warren-portfolio-router/static/media/vert2.87da8090.jpg"
+  },
+  {
+    "revision": "ccf8eed66db1d1a0aa8225e78dad7473",
+    "url": "/warren-portfolio-router/static/media/vert3.ccf8eed6.jpg"
+  },
+  {
+    "revision": "5699cfa2bfa2f1dd8ccf06f6af7ce7df",
+    "url": "/warren-portfolio-router/static/media/vert4.5699cfa2.jpg"
+  },
+  {
+    "revision": "b7651bc7b20b630d5a6cf34ae441b03d",
+    "url": "/warren-portfolio-router/static/media/vert2.b7651bc7.jpg"
+  },
+  {
+    "revision": "6d09a55e2041e73dec5a1b060a72d4b8",
+    "url": "/warren-portfolio-router/static/media/vert3.6d09a55e.jpg"
+  },
+  {
+    "revision": "e5979d7d133ec459b52e1540d3f948eb",
+    "url": "/warren-portfolio-router/static/media/vert4.e5979d7d.jpg"
+  },
+  {
+    "revision": "da54dd2575be37959f3778fa8594052d",
+    "url": "/warren-portfolio-router/static/media/GradVert2.da54dd25.jpg"
+  },
+  {
+    "revision": "70caeb73385095b9a4c42648c6cb206c",
+    "url": "/warren-portfolio-router/static/media/GradVert3.70caeb73.jpg"
+  },
+  {
+    "revision": "9630a6f33e06d0dc80b1f46a301a4107",
+    "url": "/warren-portfolio-router/static/media/vert5.9630a6f3.jpg"
+  },
+  {
+    "revision": "6882568a9c61f4f64a08ab7b6531d87e",
+    "url": "/warren-portfolio-router/static/media/vert6.6882568a.jpg"
+  },
+  {
+    "revision": "7ed9abd1c31f0f11bfba11c9e2f5109c",
+    "url": "/warren-portfolio-router/static/media/vert7.7ed9abd1.jpg"
+  },
+  {
+    "revision": "eff5bc6d7f079d66949f6d44c410cede",
+    "url": "/warren-portfolio-router/static/media/hellastudious1.eff5bc6d.jpg"
+  },
+  {
+    "revision": "0f164cd680ff4070ec6b7c20a9b664c2",
+    "url": "/warren-portfolio-router/static/media/vert1.0f164cd6.jpg"
+  },
+  {
+    "revision": "ef10f93a04e5f7b4f600307adf50ea38",
+    "url": "/warren-portfolio-router/static/media/vert2.ef10f93a.jpg"
+  },
+  {
+    "revision": "f4c68c9e8c708fb8fdb1c0cbcaf57d9c",
+    "url": "/warren-portfolio-router/static/media/vert4.f4c68c9e.jpg"
+  },
+  {
+    "revision": "556103a303c78106a6d83193c925acb3",
+    "url": "/warren-portfolio-router/static/media/horiz3.556103a3.jpg"
+  },
+  {
+    "revision": "b2a1e54e19de305d132f92717be9697b",
+    "url": "/warren-portfolio-router/static/media/vert5.b2a1e54e.jpg"
+  },
+  {
+    "revision": "6fd7c1428bd89cec7b12ccc2c55789b8",
+    "url": "/warren-portfolio-router/static/media/vert6.6fd7c142.jpg"
+  },
+  {
+    "revision": "a4fe8a2e659443113befb0317f763a27",
+    "url": "/warren-portfolio-router/static/media/vert7.a4fe8a2e.jpg"
+  },
+  {
+    "revision": "c3b5e6c233d0d8b7ec5f8c30f562ef81",
+    "url": "/warren-portfolio-router/static/media/vert8.c3b5e6c2.jpg"
+  },
+  {
+    "revision": "d0c443cc3cca7d0f80b77b49f33eaec4",
+    "url": "/warren-portfolio-router/static/media/vert9.d0c443cc.jpg"
+  },
+  {
+    "revision": "805ebe0a498cfdb183b75062750d00d8",
+    "url": "/warren-portfolio-router/static/media/vert10.805ebe0a.jpg"
+  },
+  {
+    "revision": "d9ab040c7af65b1653c410d4a6ca7736",
+    "url": "/warren-portfolio-router/static/media/vert11.d9ab040c.jpg"
+  },
+  {
+    "revision": "1ccfc0162fd4c23edeaf54eab468c15e",
+    "url": "/warren-portfolio-router/static/media/vert12.1ccfc016.jpg"
+  },
+  {
+    "revision": "71c1998849524783784dfe7e891df63d",
+    "url": "/warren-portfolio-router/static/media/vert13.71c19988.jpg"
+  },
+  {
+    "revision": "6ad989198255aa266e87651fb6be54e3",
+    "url": "/warren-portfolio-router/static/media/vert14.6ad98919.jpg"
+  },
+  {
+    "revision": "205586a73bed531d486cf4a2541df983",
+    "url": "/warren-portfolio-router/static/media/vert15.205586a7.jpg"
+  },
+  {
+    "revision": "ac1338ace689726a78f2a40d4bc10235",
+    "url": "/warren-portfolio-router/static/media/vert16.ac1338ac.jpg"
+  },
+  {
+    "revision": "b3b7fc7787767915026fbc7afce358a0",
+    "url": "/warren-portfolio-router/static/media/vert17.b3b7fc77.jpg"
+  },
+  {
+    "revision": "3774c19c9738c959e1074fef3d437f0c",
+    "url": "/warren-portfolio-router/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/warren-portfolio-router/precache-manifest.8997c35f2b1ec92fdddf025e534894b2.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/warren-portfolio-router/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
